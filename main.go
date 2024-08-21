fb10e64b214ae36049eb97f1b747f53859f7e18e
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-08-21 02:31:07 +0000

    seperated types in different file

--- main.go
+++ main.go
@@ -9,56 +9,12 @@ import (
 	"os"
 )
 
-type Event struct {
-	ID        string  `json:"id"`
-	Type      string  `json:"type"`
-	Actor     Actor   `json:"actor"`
-	Repo      Repo    `json:"repo"`
-	Payload   Payload `json:"payload"`
-	Public    bool    `json:"public"`
-	CreatedAt string  `json:"created_at"`
-}
-
-// Serve info about the users.
-type Actor struct {
-	ID   int64  `json:"id"`
-	Name string `json:"login"`
-}
-
-// Serve info about the repos.
-type Repo struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
-}
-
-type Payload struct {
-	Ref string `json:"ref"`
-	// These are for CreateEvent Type (Repositories, Tags, Branches).
-	Ref_Type      string `json:"ref_type"`
-	Master_Branch string `json:"master_branch"`
-	Description   string `json:"description"`
-	Pusher_Type   string `json:"pusher_type"`
-	// These are for PushEvent Type.
-	Repository_Id int64    `json:"repository_id"`
-	Push_Id       int64    `json:"push_id"`
-	Head          string   `json:"head"`
-	Before        string   `json:"before"`
-	Commit        []Commit `json:"commits"`
-}
-
-// Serve info about the pushed commits.
-type Commit struct {
-	Sha     string `json:"sha"`
-	Message string `json:"message"`
-}
-
 func main() {
-	var eventObject []Event
-
 	if len(os.Args) == 1 {
 		fmt.Println("Usage: ./github_activity <UserName>")
 		return
 	}
+
 	userName := os.Args[1]
 	url := fmt.Sprintf("https://api.github.com/users/%s/events/public", string(userName))
 
@@ -76,12 +32,21 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 
-	// fmt.Println(string(responseData))
+	var events []Event
 
-	err = json.Unmarshal(responseData, &eventObject)
+	err = json.Unmarshal(responseData, &events)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
 
-	fmt.Println(eventObject[0].ID)
+	for _, event := range events {
+		switch event.Type {
+		case "CreateEvent":
+			fmt.Println("CreateEvent!")
+		case "PushEvent":
+			fmt.Println("PushEvent!")
+		case "WatchEvent":
+			fmt.Println("WatchEvent!")
+		}
+	}
 }
